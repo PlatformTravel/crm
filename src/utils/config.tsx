0138c2433a9829a8fa83b76a14f9faee29472506
BTMLTECH <technology@btmlimited.net>
2026-07-08 16:27:03 +0100

    updated crm both frontnend and backend3

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -1,4 +1,4 @@
 
-// export const BACKEND_URL = 'https://crm.btmtravel.net';
-export const BACKEND_URL = 'http://localhost:8000';
+export const BACKEND_URL = 'https://crm.btmtravel.net';
+// export const BACKEND_URL = 'http://localhost:8000';
 
